454f6a7f10891b16d5d459c3ca79d2e73dbc65c6
wujiawuhuixin <442737967@qq.com>
2020-01-09 14:37:02 +0800

    手脚架搭建

---
+++ src/main/java/com/whx/shop/shop/mbg/model/User.java
@@ -0,0 +1,85 @@
+package com.whx.shop.shop.mbg.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private Integer id;
+
+    private String usernmae;
+
+    private String password;
+
+    private String gender;
+
+    private Date birthday;
+
+    private String flag;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsernmae() {
+        return usernmae;
+    }
+
+    public void setUsernmae(String usernmae) {
+        this.usernmae = usernmae;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", usernmae=").append(usernmae);
+        sb.append(", password=").append(password);
+        sb.append(", gender=").append(gender);
+        sb.append(", birthday=").append(birthday);
+        sb.append(", flag=").append(flag);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
